3e925fbe200ffe7fb06fdd0b46f3b30c95ce2bb5
parnniti <nitipanmc@gmail.com>
2022-07-20 15:12:30 +0000

    week1 save

--- server/djangobackend/settings.py
+++ server/djangobackend/settings.py
@@ -27,7 +27,10 @@ DEBUG = True
 
 APPEND_SLASH = True
 
-ALLOWED_HOSTS = ["localhost"]
+ALLOWED_HOSTS = [
+    "localhost",
+    "parnniti-djangoapp.au-syd.cf.appdomain.cloud"
+    ]
 
 
 # Application definition
